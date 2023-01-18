b75547d3879ba6ea472001e9f65aa4bf9dba7976
Kona Jaya Sowmini <44631279+sowmini4901@users.noreply.github.com>
2023-01-18 22:19:50 +0530

    Create ArrangingCoins.java

---
+++ ArrangingCoins.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/arranging-coins/description/
+
+class Solution {
+    public int arrangeCoins(int n) {
+        int count=0;
+        int i=1;
+       while(n>0){
+        n-=i;
+        i++;
+        if(n>=0)
+        count++;
+       }
+       return count;
+    }
+}

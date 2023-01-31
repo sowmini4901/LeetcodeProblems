9e74f5f13724e03be37273c46aa672a9855657a6
Kona Jaya Sowmini <44631279+sowmini4901@users.noreply.github.com>
2023-01-31 23:35:24 +0530

    Update DestinationCity.java

--- DestinationCity.java
+++ DestinationCity.java
@@ -2,9 +2,20 @@
 
 class Solution {
     public String destCity(List<List<String>> paths) {
-          HashMap<String, String> hm = new HashMap<String, String>();
+       HashMap<String, String> hm = new HashMap<String, String>();
        for(int i=0;i<paths.size();i++){
            String key = paths.get(i).get(0);
            String value = paths.get(i).get(1);
+           hm.put(key,value);
+       } 
+       String result="";
+       ArrayList<String> al = new ArrayList<String>(hm.values());
+       ArrayList<String> al2 = new ArrayList<String>(hm.keySet());
+       for(int i=0;i<al.size();i++){
+           if(!al2.contains(al.get(i))){
+            result = al.get(i);
+           }
+       }
+       return result;
     }
 }

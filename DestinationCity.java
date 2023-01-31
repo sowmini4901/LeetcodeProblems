b859f4da3906f8074f59ba0207fdf3105dd05582
Kona Jaya Sowmini <44631279+sowmini4901@users.noreply.github.com>
2023-01-31 23:28:45 +0530

    Update DestinationCity.java

--- DestinationCity.java
+++ DestinationCity.java
@@ -2,6 +2,9 @@
 
 class Solution {
     public String destCity(List<List<String>> paths) {
-        
+          HashMap<String, String> hm = new HashMap<String, String>();
+       for(int i=0;i<paths.size();i++){
+           String key = paths.get(i).get(0);
+           String value = paths.get(i).get(1);
     }
 }

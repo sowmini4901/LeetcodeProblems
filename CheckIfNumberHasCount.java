aaf5d76523203f8420dd691142cdb115652d3b89
Kona Jaya Sowmini <44631279+sowmini4901@users.noreply.github.com>
2023-02-01 22:42:48 +0530

    Create CheckIfNumberHasCount.java

---
+++ CheckIfNumberHasCount.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value/
+
+class Solution {
+    public boolean digitCount(String num) {
+        HashMap<int, int> hm = HashMap<int, int>();
+        for(int i=0;i<num.length();i++){
+           int a = num.charAt(i);
+
+        }
+    }
+}

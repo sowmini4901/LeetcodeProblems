47314f3ed31d68a0d2e6e2909480adde50002313
Kona Jaya Sowmini <44631279+sowmini4901@users.noreply.github.com>
2023-02-03 23:15:43 +0530

    Create KthDistinctStringInArray.java

---
+++ KthDistinctStringInArray.java
@@ -0,0 +1,7 @@
+//https://leetcode.com/problems/kth-distinct-string-in-an-array/
+
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashSet hs = new HashSet();
+    }
+}
